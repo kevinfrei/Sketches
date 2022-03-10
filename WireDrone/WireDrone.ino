d4c08600e883e42d4d5fedd53530e73a3f7e8039
Kevin Frei <kevinfrei@hotmail.com>
2022-03-09 20:58:34 -0800

    Basic comms working: I need to recut the PCB because I crossed RX and TX :/

--- WireDrone/WireDrone.ino
+++ WireDrone/WireDrone.ino
@@ -1,25 +1,32 @@
-#include <SoftwareSerial.h>
-
-#define RED_LED 17
-#define BLUE_LED 19
+#include "SoftwareSerial.h" 
 
 // This is for the right side:
-SoftwareSerial queen(6, 8);
+SoftwareSerial queen(PIN_SERIAL1_TX, PIN_SERIAL1_RX);
 // Dunno if the left needs to be different
 
+// #define queen Serial1
+
+
 void setup()
-{
-  pinMode(RED_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);  
-  queen.begin(230400);
+{ 
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT); 
+  queen.begin(115200);
+  /*
+   pinMode(PIN_SERIAL1_RX, INPUT_PULLUP);
+   pinMode(PIN_SERIAL1_TX, OUTPUT);
+   */
 }
 
 unsigned int last = 0;
+unsigned int number = 0;
 void loop(){
   unsigned int now = millis();
-  if (now - last > 127) {
-    byte val1 = (now >> 7) & 0xFF;
-    queen.write(val1);
-    last = now;
+  if (now - last < 500) {
+    return;
   }
+  last = now;
+  queen.write((unsigned char)(number++ & 0xFF));
+  digitalWrite(LED_RED, ((now >> 8) & 1) ? HIGH : LOW);
+  digitalWrite(LED_BLUE, ((now >> 8) & 2) ? HIGH : LOW);
 }
--- WireQueen/WireQueen.ino
+++ WireQueen/WireQueen.ino
@@ -1,24 +1,44 @@
-// #include <SoftwareSerial.h>
-#include <HardwareSerial.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
 
-#define LED 13
+// The backlight for the ST7789 display (Easier to see)
+#define LED 18
+#define TFT_CS 10
+#define TFT_DC 20
+#define TFT_RST 21
 
-//SoftwareSerial right(7, 8);
-
-#define right Serial2
+//#define right Serial2
+#define left Serial4
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 void setup()
 {
   pinMode(LED, OUTPUT);
-  right.begin(230400);
+  // right.begin(9600);
+  left.begin(115200);
+  tft.init(240, 320); // Init ST7789 320x240
+  tft.setSPISpeed(60000000);
+  tft.fillScreen(ST77XX_BLACK);
+  digitalWrite(LED, HIGH);
 }
 
 unsigned int last = 0;
 
 void loop()
 {
-  if (right.available()) {
-    byte val = right.read();
-    digitalWrite(LED, (1 == (val & 7)) ? HIGH : LOW);
+  static int16_t x1 = 0, y1 = 0;
+  static uint16_t w = 1, h = 1;
+  unsigned int now = millis();
+  while (left.available()) {
+    unsigned char b = left.read();
+    int x = (240 - w) / 2;
+    unsigned int y = (now / 200) % 280;
+    tft.fillRect(x1, y1, w, h, ST77XX_BLACK);
+    tft.getTextBounds("00", x, y, &x1, &y1, &w, &h);
+    tft.setCursor(x, y);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.print((int) b, HEX);
   }
+  delay(10);
 }
