8b5a21cd8eeb8f0fb74658aa3063f9364a22cee5
Kevin Frei <kevinfrei@hotmail.com>
2022-03-10 22:38:51 -0800

    Check every .5 seconds

--- WireQueen/WireQueen.ino
+++ WireQueen/WireQueen.ino
@@ -30,6 +30,7 @@ void loop()
   static int16_t lx1 = 0, ly1 = 0, rx1 = 0, ry1 = 0;
   static uint16_t lw = 1, lh = 1, rw = 1, rh = 1;
   unsigned int now = millis();
+  /*
   if (left.available()) {
     unsigned char b = left.read();
     tft.fillRect(lx1, ly1, lw, lh, ST77XX_BLACK);
@@ -40,15 +41,33 @@ void loop()
     tft.setTextColor((micros() ^ millis()) & 0xFFFF);
     tft.print((int) b);
   }
+  */
   if (right.available()) {
     unsigned char b = right.read();
+    uint8_t sc = b / 3;
+    uint8_t mod = b % 3;
+    uint16_t color = ST77XX_YELLOW;
+    if (sc % 3 != mod) {
+      tft.fillRect(185, 290, 55, 30, ST77XX_BLACK);
+      tft.setCursor(185, 310);
+      tft.setTextColor(ST77XX_RED);
+      tft.print((int)b);
+      color = ST77XX_WHITE;
+    } else {
+      uint16_t color = (sc < 36) ? ST77XX_RED : ST77XX_GREEN;
+      tft.fillRect((sc % 6) * 5 + 1, 160 + (sc / 6) * 5, 3, 3, color);
+    }
     tft.fillRect(rx1, ry1, rw, rh, ST77XX_BLACK);
-    uint16_t x = (micros() * now) % 223;
-    uint16_t y = (micros() * x) % 157 + 160;
+    uint16_t x = 120;
+    uint16_t y = 240;
     tft.getTextBounds("000", x, y, &rx1, &ry1, &rw, &rh);
     tft.setCursor(x, y);
-    tft.setTextColor((micros() ^ millis()) & 0xFFFF);
+    tft.setTextColor(ST77XX_WHITE);
     tft.print((int) b);
+    tft.fillRect(80, 290, 100, 30, ST77XX_BLACK);
+    tft.setCursor(80, 310);
+    tft.setTextColor(color);
+    tft.print(now);
   }
-  // delay(10);
+  delay(500);
 }
